7fd5cff69fd03533be1458e81d0ef1f50bd73f13
s s <75931684+suhasini-smy@users.noreply.github.com>
2026-01-17 15:50:51 +0530

    git

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-ver=`curl --silent http://mirror.vorboss.net/apache/tomcat/tomcat-9/ | grep v9 | awk '{split($5,c,">v") ; split(c[2],d,"/") ; print d[1]}'`
-sudo yum install -y java wget git
+set -e
+
+TOMCAT_VER=9.0.113
+TOMCAT_DIR=/opt/apache-tomcat-$TOMCAT_VER
+
+echo "Installing dependencies..."
+sudo apt-get update
+sudo apt-get install -y default-jdk wget unzip git
+
+echo "Cleaning old Tomcat..."
+sudo rm -rf /opt/apache-tomcat-9*
+
 cd /opt
-rm -rf apache*
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v$ver/bin/apache-tomcat-$ver.tar.gz
-sudo tar xvzf apache-tomcat-9.*
-sudo rm -rf apache*.gz
+
+echo "Downloading Tomcat $TOMCAT_VER..."
+sudo wget -q https://dlcdn.apache.org/tomcat/tomcat-9/v$TOMCAT_VER/bin/apache-tomcat-$TOMCAT_VER.zip
+
+echo "Extracting Tomcat..."
+sudo unzip -q apache-tomcat-$TOMCAT_VER.zip
+sudo rm -f apache-tomcat-$TOMCAT_VER.zip
+
+echo "Cloning config repo..."
 sudo git clone https://github.com/adhig93/tomcat-config
-sudo cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/manager/META-INF/context.xml
-sudo cp ./tomcat-config/context.xml /opt/apache-tomcat-9.*/webapps/host-manager/META-INF/context.xml
-sudo cp ./tomcat-config/tomcat-users.xml /opt/apache-tomcat-9.*/conf/tomcat-users.xml
+
+echo "Applying configuration..."
+sudo cp tomcat-config/context.xml $TOMCAT_DIR/webapps/manager/META-INF/context.xml
+sudo cp tomcat-config/context.xml $TOMCAT_DIR/webapps/host-manager/META-INF/context.xml
+sudo cp tomcat-config/tomcat-users.xml $TOMCAT_DIR/conf/tomcat-users.xml
+
 sudo rm -rf tomcat-config
-cd /opt/apache-tomcat-9.*
+
+echo "Starting Tomcat..."
+cd $TOMCAT_DIR
 sudo sh bin/startup.sh
+
+echo "Tomcat started successfully"
